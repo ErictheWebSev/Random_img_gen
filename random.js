11a9a28ccfbcfb33727a9de72cd03b352b7dc7bc
Eric Alfred <alfredoeric@gmail.com>
2023-09-08 13:15:19 +0100

    I just added more images and edited the css a little

--- random.js
+++ random.js
@@ -1,7 +1,7 @@
 function random() {
-    const pics = ["dawn.jpg", "keyboard.jpg", "vdark.jpg", "laptop.jpg", "img10.jpg", "Chrysanthemum.jpg", "Tulips.jpg", "img181.jpg", "code.jpg", "Jellyfish.jpg", "Penguins.jpg", "Desert.jpg", "Koala.jpg", "Lighthouse.jpg", "Hydrangeas.jpg"];
+    const pics = ['img/img.jpg', 'img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg', 'img/img4.jpg', 'img/img5.jpg', 'img/img6.jpg', 'img/img7.jpg', 'img/img8.jpg', 'img/img9.jpg', 'img/img10.jpg', 'img/img11.jpg', 'img/img12.jpg', 'img/img13.jpg', 'img/img14.jpg', 'img/img15.jpg', 'img/img16.jpg', 'img/abstrat.jpg'];
     let number = Math.floor(Math.random() * pics.length);
     let image = document.querySelector("#image");
-    console.log("clicked");
+    console.log(pics.length);
     image.src = pics[number];
 }
